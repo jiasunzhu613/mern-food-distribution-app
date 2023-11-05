6781e3984d2ec7e6b6e1af805e4a198c20805729
jonathanzhu <jiasunzhu@gmail.com>
2023-11-05 14:41:13 -0500

    Fixing JQuery

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,15 @@ const app = express();
 
 app.get('/', (request, response) => {
     console.log(request)
-    return response.status(234).send('Welcome to CAS PROJECT')
+    return response.status(234).send(`Welcome to CAS PROJECT`)
 });
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
-        console.log('App connected to database');
+        console.log(`App connected to database`);
         app.listen(PORT, () => {
-            console.log('App is listening to PORT: ${PORT}');
+            console.log(`App is listening to PORT: ${PORT}`);
         });
     })
     .catch((error) => {
