8217a96d99d0c29405329bb055ad82769faa78ab
jonathanzhu <jiasunzhu@gmail.com>
2023-11-05 13:06:54 -0500

    First commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {PORT} from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to CAS PROJECT')
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log('App is listening to PORT: ${PORT}');
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
