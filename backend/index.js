41f770b0cae348a28cceb435b4dfb30f73e2074d
jonathanzhu <jiasunzhu@gmail.com>
2023-11-14 18:49:48 -0500

    Added new express routes (find, find by id, update by id, and delete by id)

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 import {Event} from "./models/eventModel.js";
 import {User} from "./models/userModel.js";
 import * as fs from "fs";
-import * as path from "path";
 
 const app = express();
+
 
 // Allow express to parse JSON
 app.use(express.json());
@@ -15,6 +15,7 @@ app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send(`Welcome to CAS PROJECT`)
 });
+
 
 // Post events
 app.post('/event', async (request, response) => {
@@ -39,6 +40,70 @@ app.post('/event', async (request, response) => {
     }
 });
 
+// Get all events
+app.get('/event', async (request, response) => {
+    try{
+        const events = await Event.find({});
+        return response.status(200).json({
+            count : events.length,
+            data : events
+        });
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Get event by id
+app.get('/event/:id', async (request, response) => {
+    try{
+        const id = request.params.id;
+        const event = await Event.findById(id);
+        return response.status(200).json(event);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Update event
+app.put('/event/:id', async (request, response) => {
+    try{
+        if (!request.body.date || !request.body.location || !request.body.itemTypes){
+            return response.status(400).send({
+                message: `Send all required fields: date, location, itemTypes`// function to find all types required?
+            });
+        }
+        const id = request.params.id;
+        const result = await Event.findByIdAndUpdate(id, request.body);
+
+        if (!result){
+            return response.status(404).json({message : 'EEvent not found'});
+        }
+        return response.status(200).json({message : 'Event updated successfully'});
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//Delete event
+app.delete('/event/:id', async (request, response) => {
+    try{
+        const id = request.params.id;
+        const result = await Event.findOneAndDelete(id);
+
+        if (!result){
+            return response.status(404).json({message : 'Event not found'});
+        }
+        return response.status(200).json({message : 'Event deleted successfully'});
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
 // Post users
 app.post('/user', async (request, response) => {
     try {
@@ -59,6 +124,74 @@ app.post('/user', async (request, response) => {
         const user = await User.create(newUser); // Use object literal to create new user using model
         return response.status(201).send(user);
     }catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Get all users
+app.get('/user', async (request, response) => {
+    try{
+        const users = await User.find({});
+        return response.status(200).json({
+            count : users.length,
+            data : users
+        });
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Get user by id
+app.get('/user/:id', async (request, response) => {
+    try{
+        const id = request.params.id;
+        const user = await User.findById(id);
+        return response.status(200).json(user);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Update user
+app.put('/user/:id', async (request, response) => {
+    try{
+        if (!request.body.name || !request.body.icon){
+            return response.status(400).send({
+                message: `Send all required fields: name, icon`// function to find all types required?
+            });
+        }
+        const id = request.params.id;
+        const update = {
+            name: request.body.name,
+            icon: fs.readFileSync(request.body.icon, {encoding:"base64", flag:"r"}),
+            activity: request.body.activity
+        };
+        const result = await User.findByIdAndUpdate(id, update);
+
+        if (!result){
+            return response.status(404).json({message : 'User not found'});
+        }
+        return response.status(200).json({message : 'User updated successfully'});
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//Delete user
+app.delete('/user/:id', async (request, response) => {
+    try{
+        const id = request.params.id;
+        const result = await User.findOneAndDelete(id);
+
+        if (!result){
+            return response.status(404).json({message : 'User not found'});
+        }
+        return response.status(200).json({message : 'User deleted successfully'});
+    }catch(error){
         console.log(error.message);
         response.status(500).send({message: error.message});
     }
