4e17ef5d30981ef0b74c84b629c664456c830465
jonathanzhu <jiasunzhu@gmail.com>
2023-11-18 16:09:14 -0500

    Updates redundant imports

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
-import {Event} from "./models/eventModel.js";
 import eventRoute from "./routes/eventRoute.js";
-import {User} from "./models/userModel.js";
 import userRoute from "./routes/eventRoute.js";
-import * as fs from "fs";
 
 const app = express();
 
