558bf5802e2151c6b8369125057463fcb96874ea
jonathanzhu <jiasunzhu@gmail.com>
2023-11-25 13:01:22 -0500

    Added some TODO's

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,3 @@
 export const PORT = 5555;
+//TODO: prolly need to edit or gitignore this file so that info isnt leaked
 export const mongoDBURL = 'mongodb+srv://pmdjzhucas:cas123@cascluster.nbyzq0j.mongodb.net/?retryWrites=true&w=majority';
