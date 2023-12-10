0e83ed0de10774ffabcba842cbca9bffb2a04dc2
Pasindu Danthanarayana <danthe.pasindu@gmail.com>
2023-12-10 14:57:03 -0500

    Added dotenv file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
-export const PORT = 5555;
+import './loadEnv.js';
+
+export const PORT = process.env.PORT;
 //TODO: prolly need to edit or gitignore this file so that info isnt leaked
-export const mongoDBURL = 'mongodb+srv://pmdjzhucas:cas123@cascluster.nbyzq0j.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURL = process.env.MONGO_URI;
